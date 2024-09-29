973b01c4343306d713f7d526636c80af8c161394
quocan142536@gmail.com <quocan142536@gmail.com>
2024-09-30 01:20:40 +0700

    update

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -9,51 +9,54 @@ module.exports.index = async (req, res) => {
 
   console.log("QUERY::::::::");
 
-    // call find trong database
-    let find = {
-        deleted: false,
-    };
-    
-    if (req.query.status) {
-        find.status = req.query.status;
-    } // used to when click find active or inactive
-    
-    // tinh nang search
-    const objectSearch = searchHelper(req.query); // search used
+  // call find trong database
+  let find = {
+    deleted: false,
+  };
 
-    if(objectSearch.keyword){
-        find.title = objectSearch.title;
-    }
-    // end search 
+  if (req.query.status) {
+    find.status = req.query.status;
+  } // used to when click find active or inactive
 
+  // tinh nang search
+  const objectSearch = searchHelper(req.query); // search used
 
-    // pagination
-    let objectPagination = {
-        currentPage : 1,
-        limitItems : 4,
-    }    
-    if(req.query.page){
-        objectPagination.currentPage = parseInt(req.query.page);
-    }
-    
-    objectPagination.skip = (objectPagination.currentPage - 1) * objectPagination.limitItems; // calc page = (page - 1) * item page
-    
-    const countProducts = await Product.countDocuments(find); // tao bien count, dung truoc modal luon phai dung async await
+  if (objectSearch.keyword) {
+    find.title = objectSearch.title;
+  }
+  // end search
 
-    const products = await Product.find(find).limit(objectPagination.limitItems).skip(objectPagination.skip);
+  // pagination
+  //current (trang hien tai)
+  //limit (so luong muon lay moi trang)
+  let objectPagination = {
+    currentPage: 1,
+    limitItems: 2,
+  };
+  if (req.query.page) {
+    objectPagination.currentPage = parseInt(req.query.page);
+  }
 
-    const totalPage = Math.ceil(countProducts / objectPagination.limitItems); // math ceil : lam tron len
+  objectPagination.skip =
+    (objectPagination.currentPage - 1) * objectPagination.limitItems; // calc page = (page - 1) * item page
 
-    objectPagination.totalPages = totalPage; // used to link to pug
+  const countProducts = await Product.countDocuments(find); // tao bien count, dung truoc modal luon phai dung async await
 
-    // link to view
-    res.render("admin/pages/products/index", {
-     // nem bien sang pug de su dung
-        pageTitle: "Products",
-        products: products,
-        filterStatus: filterStatus,
-        keyword : objectSearch.keyword,
-        pagination: objectPagination,
-    })
-    
-}
+  const products = await Product.find(find)
+    .limit(objectPagination.limitItems)
+    .skip(objectPagination.skip);
+
+  const totalPage = Math.ceil(countProducts / objectPagination.limitItems); // math ceil : lam tron len
+
+  objectPagination.totalPage = totalPage; // used to link to pug
+
+  // link to view
+  res.render("admin/pages/products/index", {
+    // nem bien sang pug de su dung
+    pageTitle: "Products",
+    products: products,
+    filterStatus: filterStatus,
+    keyword: objectSearch.keyword,
+    pagination: objectPagination,
+  });
+};
