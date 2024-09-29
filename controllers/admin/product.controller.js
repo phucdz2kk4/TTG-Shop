06926dd547e2886936bdc29d8e5300f90d360aba
Agent P <ahshiethere123@gmail.com>
2024-09-30 01:02:30 +0700

    updatee

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -15,18 +15,35 @@ module.exports.index = async (req, res) => {
     
     if (req.query.status) {
         find.status = req.query.status;
-    }
+    } // used to when click find active or inactive
     
     // tinh nang search
     const objectSearch = searchHelper(req.query); // search used
 
-    if(objectSearch.regex){
-        find.title = objectSearch.regex;
+    if(objectSearch.keyword){
+        find.title = objectSearch.title;
     }
-
     // end search 
-    const products = await Product.find(find);
+
+
+    // pagination
+    let objectPagination = {
+        currentPage : 1,
+        limitItems : 4,
+    }    
+    if(req.query.page){
+        objectPagination.currentPage = parseInt(req.query.page);
+    }
     
+    objectPagination.skip = (objectPagination.currentPage - 1) * objectPagination.limitItems; // calc page = (page - 1) * item page
+    
+    const countProducts = await Product.countDocuments(find); // tao bien count, dung truoc modal luon phai dung async await
+
+    const products = await Product.find(find).limit(objectPagination.limitItems).skip(objectPagination.skip);
+
+    const totalPage = Math.ceil(countProducts / objectPagination.limitItems); // math ceil : lam tron len
+
+    objectPagination.totalPages = totalPage; // used to link to pug
 
     // link to view
     res.render("admin/pages/products/index", {
@@ -34,7 +51,8 @@ module.exports.index = async (req, res) => {
         pageTitle: "Products",
         products: products,
         filterStatus: filterStatus,
-        keyword : objectSearch.keyword
+        keyword : objectSearch.keyword,
+        pagination: objectPagination,
     })
     
 }
